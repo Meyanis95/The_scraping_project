c5b9ac3727792f82f7580abedb732cdd44a1c055
Meyanis95 <b00694485@essec.edu>
2020-07-16 22:01:41 +0200

    Fini

--- spec/mairie_christmas_rspec.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../lib/mairie_christmas.rb'
-
-describe "the get_townhall_urls method" do
-    it "should return an array of urls of each towns" do
-      expect(get_townhall_urls).not_to eq(nil)
-    end
-end
